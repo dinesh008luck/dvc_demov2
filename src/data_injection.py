3cfd2f72af0c7d9ccd75eab8e6657c1076ac718f
Dinesh Kumar <dinesh008luck@gmail.com>
2026-03-15 16:37:54 +0530

    inital commit

---
+++ src/data_injection.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import os
+
+df= pd.read_csv(r'C:\Users\SLS0822\Downloads\sqllab_sap_live_mtd_sales_20260314T153733.csv')
+
+df= df.iloc[:,3:]
+
+df= df.pivot_table(index='Customer Code',
+                   columns='Sales UOM',
+                   values='Quantity Sold',
+                   aggfunc = 'sum',
+                   
+                   )
+
+df.to_csv(os.path.join('data','sales.csv'))
+

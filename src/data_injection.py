2898abf83318776f26d503a2897636b7583d3174
Dinesh Kumar <dinesh008luck@gmail.com>
2026-03-15 18:02:31 +0530

    second expreriments

--- src/data_injection.py
+++ src/data_injection.py
@@ -6,12 +6,13 @@ df= pd.read_csv(r'C:\Users\SLS0822\Downloads\sqllab_sap_live_mtd_sales_20260314T
 
 df= df.iloc[:,3:]
 
+df= df [df['Quantity Sold'] >10]
+
 df= df.pivot_table(index='Customer Code',
                    columns='Sales UOM',
                    values='Quantity Sold',
                    aggfunc = 'sum',
                    fill_value=0
                    )
-
 df.to_csv(os.path.join('data','sales.csv'))
 
